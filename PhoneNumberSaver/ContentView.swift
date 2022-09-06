154b74d927b279124079be6db02e692b20bcde32
98dimdim <dimas.prabowo@tokopedia.com>
2022-09-06 14:55:52 +0700

    Initial Commit

---
+++ PhoneNumberSaver/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PhoneNumberSaver
+//
+//  Created by dimas.prabowo on 06/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PhoneNumberSaver/PhoneNumberSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneNumberSaverApp.swift
+//  PhoneNumberSaver
+//
+//  Created by dimas.prabowo on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PhoneNumberSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
